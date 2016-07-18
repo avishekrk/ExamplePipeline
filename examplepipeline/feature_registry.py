bc11dc761d9ddba6a128d72744803329a25192e4
Kevin Wilson <khwilson@gmail.com>
2016-07-17 22:39:15 -0500

    Now with a Feature Registry

--- examplepipeline/feature_registry.py
+++ examplepipeline/feature_registry.py
@@ -3,7 +3,23 @@ This file is used to maintain a list of all features that are available
 to the user to be created. By default, features are created in the order
 they appear in this registry.
 """
+from __future__ import absolute_import
 
-from .features.simple_features import RowKeyFeature, AddTwoFeature
+import luigi
 
-FEATURE_REGISTRY = (RowKeyFeature(), AddTwoFeature())
+from . import luigi_features
+from .features import simple_features
+
+FEATURE_REGISTRY = (simple_features.RowKeyFeature(), simple_features.AddTwoFeature())
+
+class LuigiFeatureRegistry(luigi.Task):
+    """ Register your classes here in requires """
+
+    def requires(self):
+        return [luigi_features.RowKeyFeature(), luigi_features.AddTwoFeature()]
+
+    def output(self):
+        pass
+
+    def run(self):
+        pass
